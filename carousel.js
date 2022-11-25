4c9034af7cd5aba141ffac08447eebb3a92746de
cameo choquer <cjchoquer@gmail.com>
2022-11-25 19:34:43 +0000

    add race day, recentered activity section + button

--- carousel.js
+++ carousel.js
@@ -1,11 +1,9 @@
 
           const d = new Date().toDateString();
           document.getElementById('date-time').innerHTML = "Current Date: " + d;
-       
-// function myFunction() {
-//   var w = window.open();
-//   w.document.open(<a href="comment.html">);
-//   w.document.write("<h2>Hello World!</h2>");
-//   w.document.close();
-// };
+          
+
+function myFunction(){
+  var w = window.open("https://github.com/cameochoquer/fac_proj_2");
+};
         
