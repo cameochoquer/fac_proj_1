b85d39e4d3a7fdad685146824a104d41afc1eb23
cameo choquer <cjchoquer@gmail.com>
2022-11-27 14:06:57 +0000

    updated html with new js file name

--- Date.js
+++ Date.js
@@ -1,8 +1,10 @@
+
+
 const d = new Date().toDateString();
 document.getElementById('date-time').innerHTML = "Current Date: " + d;
           
 
 function myFunction(){
-  var w = window.open("https://github.com/cameochoquer/fac_proj_2");
-};
+var w = window.open("https://github.com/cameochoquer/fac_proj_2");
+ };
         
